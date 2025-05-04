2d99bdaab4607db444f7039a2a51272070f38d77
Kacper Jarosławski <kacper.jaroslawski@kzl21.ovh>
2025-05-04 13:27:24 +0200

    Initial Commit

---
+++ BatteryChecker/BatteryCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryCheckerApp.swift
+//  BatteryChecker
+//
+//  Created by Kacper Jarosławski on 04/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct BatteryCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BatteryCheckerTests/BatteryCheckerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryCheckerTests.swift
+//  BatteryCheckerTests
+//
+//  Created by Kacper Jarosławski on 04/05/2025.
+//
+
+import Testing
+@testable import BatteryChecker
+
+struct BatteryCheckerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
