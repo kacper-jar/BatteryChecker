66b52a10c5dac66e82878b8ae3db4a733746097d
Kacper Jarosławski <kacper.jaroslawski@kzl21.ovh>
2025-05-04 16:55:02 +0200

    Add Quit button and autostart

--- BatteryChecker/ContentView.swift
+++ BatteryChecker/ContentView.swift
@@ -6,34 +6,61 @@
 //
 
 import SwiftUI
+import ServiceManagement
 
 struct ContentView: View {
     @AppStorage("prepareThreshold") var prepareThreshold = 40
     @AppStorage("chargeNowThreshold") var chargeNowThreshold = 30
-    
+    @AppStorage("launchAtLogin") private var launchAtLogin: Bool = false
+
     var body: some View {
         VStack(spacing: 20) {
-            Text("Battery Notification Settings")
+            Text("Battery Checker")
                 .font(.headline)
-            
+
             Text("By Kacper Jarosławski")
                 .font(.subheadline)
                 .foregroundColor(.gray)
-            
+
             Stepper("Prepare Charger at: \(prepareThreshold)%", value: $prepareThreshold, in: 10...100)
-            
+
             Stepper("Charger Needed at: \(chargeNowThreshold)%", value: $chargeNowThreshold, in: 5...prepareThreshold - 1)
-            
+
+            Toggle("Start at login", isOn: $launchAtLogin)
+                .onChange(of: launchAtLogin) { newValue in
+                    setLaunchAtLogin(enabled: newValue)
+                }
+
             Text("Settings saved automatically")
                 .font(.footnote)
                 .foregroundColor(.gray)
-            
-            Link("GitHub", destination: URL(string: "https://github.com/kacper-jar")!)
+
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }
+            .keyboardShortcut("q")
+            .padding(.top, 10)
+
+            Link("GitHub", destination: URL(string: "https://github.com/kacper-jar/BatteryChecker")!)
                 .font(.footnote)
                 .foregroundColor(.blue)
         }
         .padding()
         .frame(width: 300)
+    }
+}
+
+func setLaunchAtLogin(enabled: Bool) {
+    do {
+        if enabled {
+            try SMAppService.mainApp.register()
+            print("Autostart enabled")
+        } else {
+            try SMAppService.mainApp.unregister()
+            print("Autostart disabled")
+        }
+    } catch {
+        print("Failed to change launch at login setting: \(error)")
     }
 }
 
